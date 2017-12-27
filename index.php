ae1011dfc088f4669d5f4a9fa017b6224226afc8
hoangkuty <ageofdarknesstinker@gmail.com>
2017-12-27 23:20:29 +0700

    -test first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Web bán sách</title>
+            <link rel="stylesheet" type="text/css" href="style/style.css" ">
+        </head>
+
+        <body>
+            <div class="wrapper">This is wrapper
+                <div class="header">This is header</div>
+                <div class="content">This is header</div>
+                <div class="footer">This is footer</div>
+            </div>
+        </body>
+    </html>
